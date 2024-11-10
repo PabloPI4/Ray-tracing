42779c1d9a03f80d97fd5e7ecef53748a266f7de
Bruno <brunoburkos@gmail.com>
2024-11-10 15:05:02 +0100

    formats

--- ray-tracing.c
+++ ray-tracing.c
@@ -9,9 +9,9 @@ FILE* image;
 
 int main(int argc, char *argv[]) {
     image = fopen("image.ppm","w");
-    if(!image) {
-	perror("fopen");
-        return 3;
+    if(!image) { /*error with the end image*/
+	    perror("fopen");
+        exit(3);
     }
     readObjects(argc, argv);
 }
@@ -20,89 +20,85 @@ void readObjects(int argc, char *argv[]) {
     FILE *readObjects;
     double pos[3];
     double r;
-
+    int end,i;
+    char stdbol = 0; 
     if (argc < 2) {
         readObjects = stdin;
+        stdbol = 1;
     }
     else if (argc == 2) {
-        printf("Objects are going to be readed from file named: %s\n", argv[1]);
+        printf("Objects are going to be readed from the file named: %s\n", argv[1]);
         readObjects = fopen(argv[1], "r");
         if (readObjects == NULL) {
-            fprintf(stderr, "Error opening file named: %s\n", argv[1]);
+            perror("fprintf");
+            fprintf(stderr, "Error opening the file named: %s\n", argv[1]);
         }
     }
     else {
-        fprintf(stderr, "Number of arguments incorrect. Passed %d arguments and this program only need 1\n", argc - 1);
+        fprintf(stderr, "Incorrect number of arguments. Passed %d arguments while this program needs just 1\n", argc - 1);
     }
-
-    printf("Write the position of the camera: x y z vector(x) vector(y) vector(z)\n");
+    if(stdbol) printf("Write the camera's position. Format: x y z vector(x) vector(y) vector(z)\n");
     if (fscanf(readObjects, "%lf%lf%lf%lf%lf%lf", camera.center, camera.center + 1, camera.center + 2, camera.direction, camera.direction + 1, camera.direction + 2) < 6) {
         fprintf(stderr, "Error reading camera\n");
         exit(1);
     }
-
-    printf("Write the position of the light: x y z\n");
+    if(stdbol) printf("Write the light's position. Format: x y z\n");
     if (fscanf(readObjects, "%lf%lf%lf", light, light + 1, light + 2) < 3) {
         fprintf(stderr, "Error reading camera\n");
         exit(2);
     }
-
-    int end = 0;
+    end = 0;
     while(!end) {
-        printf("Reading the center and radius of the spheres: x y z r\n");
+        if(stdbol) printf("Reading the center and radius of the spheres. Format: x y z r\n");
         if (fscanf(readObjects, "%lf%lf%lf%lf", pos, pos + 1, pos + 2, &r) < 4) {
-            fprintf(stderr, "End of reading\n");
+            if(stdbol) fprintf(stderr, "End of reading\n");
             end = 1;
         }
         else {
-            spheres = (sph *) realloc(spheres, ++n_spheres * sizeof(struct sphere));
-            for (int i = 0; i < 3; i++) {
+            spheres = (sph*) realloc(spheres, ++n_spheres * sizeof(sph));
+            for (i = 0; i < 3; i++) {
                 spheres[n_spheres - 1].center[i] = pos[i];
             }
             spheres[n_spheres - 1].r = r;
         }
     }
+    if(stdbol) fclose(readObjects);
 }
+
 ln *calculateReflex(ln *ray, sph *sphere, double point[3]) {
     double vectorStdSph[3];
     double newPoint[3];
-
     for (int i = 0; i < 3; i++) {
         vectorStdSph[i] = (point[i] - sphere->center[i])/sphere->r;
         newPoint[i] = 2*(point[i] + vectorStdSph[i]) - (point[i] - ray->vector[i]);
     }
-
     return calculateRay(point, newPoint, ray);
 }
 
 ln *calculateRay(double point1[3], double point2[3], ln *ray) {
-    if (ray == NULL) {
+    double std;
+    if (!ray) { /*ray == NULL*/
         if ((ray = (ln *) malloc(sizeof(ln))) == NULL) {
             perror("malloc");
             exit(3);
         }
     }
-
     for (int i = 0; i < 3; i++) {
         ray->startPoint[i] = point1[i];
         ray->vector[i] = point2[i] - point1[i];
     }
-
-    double std = scalarProduct(ray->vector, ray->vector);
+    std = scalarProduct(ray->vector, ray->vector);
     for (int i = 0; i < 3; i++) {
         ray->vector[i] /= std;
     }
-
     return ray;
 }
 
 double calculateDistance(double point1[3], double point2[3]) {
     double vectorDistance[3];
-
     for (int i = 0; i < 3; i++) {
         vectorDistance[i] = point1[i] - point2[i];
     }
-
     return scalarProduct(vectorDistance, vectorDistance);
 }
 
